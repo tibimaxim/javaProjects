5308ebe91c677baf69fbc1007f5181cea103093f
phatom <phatom@ubuntu>
2017-11-05 22:42:28 +0200

    switched to mysql and added csrf protection

--- csv/src/main/java/ro/tibi/csv/security/WebSecurityConfig.java
+++ csv/src/main/java/ro/tibi/csv/security/WebSecurityConfig.java
@@ -5,19 +5,29 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 @Override
-	 protected void configure(HttpSecurity http) throws Exception {
-	 http.authorizeRequests()
-	 .antMatchers("/app/*").authenticated()
-     .and().formLogin().loginPage("/index.html").defaultSuccessUrl("/app/").failureUrl("/index.html?op=invalidCredentials").permitAll()
-     .and().httpBasic().and().csrf().disable();
-	 }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/app/*").authenticated().and().formLogin().loginPage("/index.html")
+				.defaultSuccessUrl("/app/").failureUrl("/index.html?op=invalidCredentials").permitAll().and()
+				.authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().and()
+				.httpBasic().and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class).csrf()
+				.csrfTokenRepository(csrfTokenRepository());
+		;
+	}
 
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
 
 }
